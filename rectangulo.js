1b9baa291af0a3cfe68c46556fb859202060bfec
Rayhsvr <rayhsvr@gmail.com>
2021-04-19 20:10:43 -0500

    Calcular ára de rectángulo

---
+++ rectangulo.js
@@ -0,0 +1,9 @@
+let nombre = prompt("Proporciona nombre: ")
+let lado1 = prompt("Proporciona lado 1: ")
+let lado2 = prompt("Proporciona lado 2: ")
+let area=0
+area = parseInt(lado1)*parseInt(lado2)
+document.write('<h1 style = "text-align: center;">Bienvenido '+ nombre + '</h1>')
+document.write('<p>Lado 1 = ' + lado1 + '</p>')
+document.write('<p>Lado 2 = ' + lado2 + '</p>')
+document.write('<p style = "font-weight:bold;">Area = ' + area + '</p>')
